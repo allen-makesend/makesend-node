7d650db2ed2e394953a4252c182d380cbf367923
allenlin90 <ht016746@gmail.com>
2021-07-06 21:18:15 +0700

    add endpoint for iii

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/tripleiii', async (req, res) => {
+    try {
+        const url = `https://apiold.makesend.asia/api/google/makesend/getSaleRegisterLeadResultList`;
+        const { data } = await axios.post(url);
+        const list = data.saleReferralList.filter(client => {
+            return parseInt(client.saleID) === 11; // 11 is dedicated iii user referral ID
+        });
+        console.log(list);
+        res.send(JSON.stringify(list));
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    console.log(`Server starts at port ${PORT}`);
+});
