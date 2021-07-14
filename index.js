e66fca02405daac1d57b7ff17c2191e6e2e6c629
allenlin90 <ht016746@gmail.com>
2021-07-14 20:14:17 +0700

    add one route for namwan user 12

--- index.js
+++ index.js
@@ -16,7 +16,21 @@ app.get('/tripleiii', async (req, res) => {
     } catch (err) {
         res.status(500).send(err);
     }
-})
+});
+
+app.get('/namwan', async (req, res) => {
+    try {
+        const url = `https://apiold.makesend.asia/api/google/makesend/getSaleRegisterLeadResultList`;
+        const { data } = await axios.post(url);
+        const list = data.saleReferralList.filter(client => {
+            return parseInt(client.saleID) === 11; // 12 is dedicated namwan user referral ID
+        });
+        console.log(list);
+        res.send(JSON.stringify(list));
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 app.listen(PORT, (err) => {
     if (err) console.log(err);
