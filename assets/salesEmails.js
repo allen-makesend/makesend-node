635e492c2aa6efc6ba3e6abfefdcc72cf1d215e5
allenlin90 <ht016746@gmail.com>
2021-07-30 10:48:58 +0700

    add routes for MS fast sales

---
+++ assets/salesEmails.js
@@ -0,0 +1,56 @@
+const salesEmails = {
+    'linmting@airportels.asia': {
+        email: 'linmting@airportels.asia',
+        salesId: 3,
+    },
+    'royce@airportels.asia': {
+        email: 'royce@airportels.asia',
+        salesId: 4,
+    },
+    'nonlanee@makesend.asia': {
+        email: 'nonlanee@makesend.asia',
+        salesId: 5,
+    },
+    'prachya.kh@makesend.asia': {
+        email: 'prachya.kh@makesend.asia',
+        salesId: 6,
+    },
+    'levihuang@gmail.com': {
+        email: 'levihuang@gmail.com',
+        salesId: 7,
+    },
+    'wasan@makesend.asia': {
+        email: 'wasan@makesend.asia',
+        salesId: 8,
+    },
+    'atcharaporn@team.makesend.asia': {
+        email: 'atcharaporn@team.makesend.asia',
+        salesId: 9,
+    },
+    'kamonrat@team.makesend.asia': {
+        email: 'kamonrat@team.makesend.asia',
+        salesId: 10,
+    },
+    'royce@makesend.asia': {
+        email: 'royce@makesend.asia',
+        salesId: 11,
+    },
+    'whan19.tb@gmail.com': {
+        email: 'whan19.tb@gmail.com',
+        salesId: 12,
+    },
+    'gf.ps.online@gmail.com': {
+        email: 'gf.ps.online@gmail.com',
+        salesId: 13,
+    },
+    '1234567yokyok@gmail.com': {
+        email: '1234567yokyok@gmail.com',
+        salesId: 14,
+    },
+    'supattrapanna09@gmail.com': {
+        email: 'supattrapanna09@gmail.com',
+        salesId: 15,
+    },
+};
+
+module.exports = salesEmails;
--- index.js
+++ index.js
@@ -1,8 +1,70 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+// import sales emails
+const salesEmails = require('./assets/salesEmails.js');
+
+app.post('/fast/login', async (req, res) => {
+    try {
+        if (req.body.email.toLowerCase() in salesEmails) {
+            res.send(JSON.stringify({email: req.body.email}));
+        } else {
+            res.status(401).send(false);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(JSON.stringify(err));
+    }
+});
+
+app.post('/fast/logout', async (req, res) => {
+    try {
+        res.send('logged out');
+    } catch (error) {
+        res.status(500).send(JSON.stringify(error));
+    }
+});
+
+app.post('/fast/auth', async (req, res) => {
+    try {
+        if (req.headers.authorization) {
+            const credentials = req.headers.authorization.split(' ')[1];
+            if (credentials in salesEmails) {
+                res.send(JSON.stringify({user: salesEmails[credentials]}));
+            } else {
+                res.status(401).send(false);
+            }
+        } else {
+            res.status(401).send(false);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(JSON.stringify(err));
+    }
+});
+
+app.post('/fast/salesperformance', async(req, res) => {
+    try {
+        const {salesId} = req.body;
+        const url = `https://apiold.makesend.asia/api/google/makesend/getSaleRegisterLeadResultList`;
+        const { data } = await axios.post(url);
+        const list = data.saleReferralList.filter(client => {
+            return parseInt(client.saleID) === salesId; 
+        });
+        res.send(JSON.stringify(list));
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 app.get('/tripleiii', async (req, res) => {
     try {
